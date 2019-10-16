8183ecc64b2937eabcb3d0a24020fe5712f4224f
Andrey Timoshin <freempt1@gmail.com>
2019-10-16 10:54:47 +0300

    base commit

---
+++ precache-manifest.fce9eaf8d7b82bc4cf317fde85ac38be.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97020a7bce098b9341d8f412f91303f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "703259c0a7b134127fda",
+    "url": "/static/js/2.33115d86.chunk.js"
+  },
+  {
+    "revision": "cec7fe85ebea726b7574",
+    "url": "/static/js/main.550192b7.chunk.js"
+  },
+  {
+    "revision": "d3be43f82b40ba83361e",
+    "url": "/static/js/runtime~main.d5d1f504.js"
+  }
+]);
